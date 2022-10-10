dc63d096758afab098e92da5914064531305e626
loss23 <mebob248888@gmail.com>
2022-10-10 02:29:12 -0500

    Stuff

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 // Vars \\
 
+var Slope_Button = document.getElementById("Slope_Button");
 
 // Animations \\
 
 
 
 // Functions \\
+
+function Open_Slop(){
+    window.location.href = "/SLOPE/Index.html";
+}
