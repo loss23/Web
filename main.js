e65af81605b57c4e9f78bddcb203af13d05edef5
loss23 <mebob248888@gmail.com>
2022-10-11 01:35:21 -0500

    f

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-// Vars \\
-
-var Slope_Button = document.getElementsByClassName("Slope_Button")
-
-
 // Events \\
 
-Slope_Button.addEventListener('click', function handleClick() {
-    console.log('element clicked');
-  });
+var SlopeButton = document.getElementById("SlopeButton");
+SlopeButton.addEventListener("click", function(event){
+   window.location.href = "games/SLOPE/Index.html"
+});
